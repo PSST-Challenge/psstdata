a2313dbdd9ab5ef2e4832bae9b5de85dd177dd5b
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-29 07:55:23 +0000

    Adding tarfile member sanitization to extractall()

--- psstdata/downloading.py
+++ psstdata/downloading.py
@@ -72,7 +72,26 @@ def _download_split(destination_folder, split: str, url: str):
             def itermembers():
                 for tarinfo in t:
                     yield tarinfo
-            t.extractall(incomplete, itermembers())
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(t, incomplete, itermembers())
 
     shutil.move(os.path.join(incomplete, split), destination_folder)
     os.rmdir(incomplete)
